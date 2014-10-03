69d0d42ec134eb06c283c2185f937b34fc070007
Dominique Menu <dominique.menu@gmail.com>
2014-10-03 21:14:35 +0200

    Script & Style bundles

--- JunaDigitalWebsite/App_Start/BundleConfig.cs
+++ JunaDigitalWebsite/App_Start/BundleConfig.cs
@@ -8,21 +8,22 @@ namespace JunaDigitalWebsite
     // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
     public static void RegisterBundles(BundleCollection bundles)
       {
-      bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                  "~/Scripts/jquery-{version}.js"));
-
-      // Use the development version of Modernizr to develop with and learn from. Then, when you're
-      // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-      bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                  "~/Scripts/modernizr-*"));
-
-      bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                "~/Scripts/bootstrap.js",
-                "~/Scripts/respond.js"));
+      bundles.Add(new ScriptBundle("~/Scripts/bundle").Include(
+                                              "~/Scripts/jquery-{version}.js",
+                                              "~/Scripts/bootstrap.js",
+                                              "~/Scripts/jquery.easing.min.js",
+                                              "~/Scripts/jquery.scrollTo.js",
+                                              "~/Scripts/wow.min.js",
+                                              "~/Scripts/respond.js",
+                                              "~/Scripts/custom.js",
+                                              "~/Scripts/site.js"));
 
       bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/bootstrap.css",
-                "~/Content/site.css"));
+                                            "~/Content/bootstrap.css",
+                                            "~/Content/font-awesome.css",
+                                            "~/Content/animate.css",
+                                            "~/Content/style.css",
+                                            "~/Content/color.css"));
       }
     }
   }
