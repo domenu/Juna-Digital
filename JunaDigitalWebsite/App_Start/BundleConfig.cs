279bcac36c7ad3a8b01e4a45177240c32fd1ba0f
Dominique Menu <dominique.menu@gmail.com>
2014-10-04 21:49:51 +0200

    GA for production

--- JunaDigitalWebsite/App_Start/BundleConfig.cs
+++ JunaDigitalWebsite/App_Start/BundleConfig.cs
@@ -1,22 +1,30 @@
-﻿using System.Web;
+﻿using System.Configuration;
+using System.Web;
 using System.Web.Optimization;
 
 namespace JunaDigitalWebsite
-  {
+{
   public class BundleConfig
-    {
+  {
     // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
     public static void RegisterBundles(BundleCollection bundles)
+    {
+      var scriptBundle = new ScriptBundle("~/Scripts/bundle").Include(
+                                                    "~/Scripts/jquery-{version}.js",
+                                                    "~/Scripts/bootstrap.js",
+                                                    "~/Scripts/jquery.easing.min.js",
+                                                    "~/Scripts/jquery.scrollTo.js",
+                                                    "~/Scripts/wow.min.js",
+                                                    "~/Scripts/respond.js",
+                                                    "~/Scripts/custom.js",
+                                                    "~/Scripts/site.js");
+
+      if (ConfigurationManager.AppSettings["Environment"] == "Release")
       {
-      bundles.Add(new ScriptBundle("~/Scripts/bundle").Include(
-                                              "~/Scripts/jquery-{version}.js",
-                                              "~/Scripts/bootstrap.js",
-                                              "~/Scripts/jquery.easing.min.js",
-                                              "~/Scripts/jquery.scrollTo.js",
-                                              "~/Scripts/wow.min.js",
-                                              "~/Scripts/respond.js",
-                                              "~/Scripts/custom.js",
-                                              "~/Scripts/site.js"));
+        scriptBundle.Include("~/Scripts/ga.js");
+      }
+
+      bundles.Add(scriptBundle);
 
       bundles.Add(new StyleBundle("~/Content/css").Include(
                                             "~/Content/bootstrap.css",
@@ -24,6 +32,6 @@ namespace JunaDigitalWebsite
                                             "~/Content/animate.css",
                                             "~/Content/style.css",
                                             "~/Content/color.css"));
-      }
     }
   }
+}
